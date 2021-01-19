f9811619d4e8dd618dc63ddea0de085a0004468f
Deepak Emani <deepakemani@gmail.com>
2021-01-19 20:10:37 +0000

    spring boot app base

---
+++ src/main/java/com/deepak/springbootreactiveappone/SpringBootReactiveAppOneApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.springbootreactiveappone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveAppOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveAppOneApplication.class, args);
+	}
+
+}
